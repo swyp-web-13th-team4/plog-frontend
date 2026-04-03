9081571155f82e0da7f674cea971ca0f58c707dc
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-03 21:25:57 +0900

    chore: apps/web Jest + RTL 테스트 환경 추가

---
+++ apps/web/jest.config.js
@@ -0,0 +1,11 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({ dir: './' });
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+};
+
+module.exports = createJestConfig(config);
