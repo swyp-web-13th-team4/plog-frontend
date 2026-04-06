2fe11c609f7e537d3e259fe79f754151e120dee7
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-06 22:33:04 +0900

    chore: eslint-plugin-react 추가 및 JSX 관련 설정 보완

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import baseConfig from '@plog/config/eslint/base';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -13,12 +14,17 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    settings: {
+      react: { version: 'detect' },
     },
   },
   ...baseConfig,
