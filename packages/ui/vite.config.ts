e310c14702822965a1b6231090b64195bc211791
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-08 00:06:41 +0900

    chore: @plog/ui 라이브러리 빌드 환경 구성

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts({ include: ['src'], rollupTypes: true })],
+  plugins: [tailwindcss(), react(), dts({ include: ['src'], rollupTypes: true, tsconfigPath: './tsconfig.app.json' })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
