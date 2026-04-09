7c531619c55fd6a7cf01437c4d8e31f305bd051e
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-10 02:51:49 +0900

    chore: Storybook addon-docs 추가

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -4,7 +4,11 @@ process.env.STORYBOOK = 'true';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-onboarding', '@chromatic-com/storybook'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-docs',
+    '@chromatic-com/storybook',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
