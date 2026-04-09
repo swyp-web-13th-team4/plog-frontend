5ef182d7f824a437a17e5f34ed21c2484a29fc01
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-10 03:47:20 +0900

    feat: Button focus-visible 스타일 추가

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -7,21 +7,24 @@ import GraySpinner from '@/assets/gray-spinner.svg?react';
 import WhiteSpinner from '@/assets/white-spinner.svg?react';
 
 const buttonVariants = cva(
-  'inline-flex cursor-pointer items-center justify-center transition-colors disabled:cursor-not-allowed',
+  'inline-flex cursor-pointer items-center justify-center transition-colors disabled:cursor-not-allowed focus-visible:outline-2',
   {
     variants: {
       variant: {
         primary:
-          'bg-semantic-accent-normal text-semantic-object-inverse hover:bg-semantic-accent-neutral active:bg-semantic-accent-bold disabled:bg-semantic-object-subtle',
+          'bg-semantic-accent-normal text-semantic-object-inverse hover:bg-semantic-accent-neutral active:bg-semantic-accent-bold disabled:bg-semantic-object-subtle focus-visible:outline-semantic-accent-subtle',
         secondary:
-          'bg-semantic-object-subtler text-semantic-object-bold hover:bg-semantic-bg-deep hover:text-semantic-object-normal active:bg-semantic-object-subtle active:text-semantic-object-bold disabled:bg-semantic-object-subtler disabled:text-semantic-object-subtle',
+          'bg-semantic-object-subtler text-semantic-object-bold hover:bg-semantic-bg-deep hover:text-semantic-object-normal active:bg-semantic-object-subtle active:text-semantic-object-bold disabled:bg-semantic-object-subtler disabled:text-semantic-object-subtle focus-visible:outline-semantic-stroke-subtle',
         outline:
-          'border border-semantic-stroke-assistive bg-semantic-system-white text-semantic-object-normal hover:bg-semantic-bg-deep active:bg-semantic-object-subtler disabled:bg-semantic-bg-deeper disabled:text-semantic-object-subtle',
+          'border border-semantic-stroke-assistive bg-semantic-system-white text-semantic-object-normal hover:bg-semantic-bg-deep active:bg-semantic-object-subtler disabled:bg-semantic-bg-deeper disabled:text-semantic-object-subtle focus-visible:outline-semantic-stroke-subtle',
       },
       size: {
-        large: 'label-lg rounded-[12px] px-5 py-3 gap-2.5 [&>svg]:size-6',
-        medium: 'label-md rounded-[12px] px-4 py-3 gap-2 [&>svg]:size-5',
-        small: 'label-sm rounded-[8px] px-3 py-2 gap-1.5 [&>svg]:size-4',
+        large:
+          'label-lg rounded-[12px] px-5 py-3 gap-2.5 [&>svg]:size-6 focus-visible:outline-offset-2',
+        medium:
+          'label-md rounded-[12px] px-4 py-3 gap-2 [&>svg]:size-5 focus-visible:outline-offset-2',
+        small:
+          'label-sm rounded-[8px] px-3 py-2 gap-1.5 [&>svg]:size-4 focus-visible:outline-offset-1',
       },
       fullWidth: {
         true: 'w-full',
