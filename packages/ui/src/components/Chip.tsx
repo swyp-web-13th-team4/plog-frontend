c949ac42c40e6ed55223bcabd220c8df29db7190
백승민 <jah02190@naver.com>
2026-04-13 22:19:22 +0900

    fix: Chip 컴포넌트의 사이즈 스타일 수정

--- packages/ui/src/components/Chip.tsx
+++ packages/ui/src/components/Chip.tsx
@@ -14,8 +14,8 @@ const chipVariants = cva(
         true: 'bg-semantic-accent-normal text-semantic-object-inverse enabled:hover:bg-semantic-accent-bold enabled:active:bg-semantic-accent-bolder disabled:bg-semantic-object-subtler disabled:text-semantic-object-subtle focus-visible:outline-semantic-accent-subtle',
       },
       size: {
-        sm: 'label-sm gap-1.5 px-3 py-2 [&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:fill-current',
-        lg: 'label-lg gap-2.5 px-4 py-3 [&_svg]:size-5 [&_svg]:shrink-0 [&_svg]:fill-current',
+        sm: 'label-sm gap-1.5 px-3 py-2 [&_svg]:size-3.25 [&_svg]:shrink-0 [&_svg]:fill-current',
+        lg: 'label-lg gap-2.5 px-4 py-3 [&_svg]:size-3.25 [&_svg]:shrink-0 [&_svg]:fill-current',
       },
     },
     defaultVariants: {
