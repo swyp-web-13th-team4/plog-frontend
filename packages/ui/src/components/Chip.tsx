207e9f2051c5a80b5d5a70edfd2a597286cfd1a2
백승민 <jah02190@naver.com>
2026-04-15 13:09:58 +0900

    fix: Chip 컴포넌트의 타입 및 스타일 수정

--- packages/ui/src/components/Chip.tsx
+++ packages/ui/src/components/Chip.tsx
@@ -1,4 +1,8 @@
-import { type ComponentProps, forwardRef, type ReactNode } from 'react';
+import {
+  type ComponentPropsWithoutRef,
+  forwardRef,
+  type ReactNode,
+} from 'react';
 
 import { Button as BaseButton } from '@base-ui/react/button';
 import { cn } from '@plog/utils';
@@ -14,8 +18,8 @@ const chipVariants = cva(
         true: 'bg-semantic-accent-normal text-semantic-object-inverse enabled:hover:bg-semantic-accent-bold enabled:active:bg-semantic-accent-bolder disabled:bg-semantic-object-subtler disabled:text-semantic-object-subtle focus-visible:outline-semantic-accent-subtle',
       },
       size: {
-        sm: 'label-sm gap-1.5 px-3 py-2 [&_svg]:size-3.25 [&_svg]:shrink-0 [&_svg]:fill-current',
-        lg: 'label-lg gap-2.5 px-4 py-3 [&_svg]:size-3.25 [&_svg]:shrink-0 [&_svg]:fill-current',
+        sm: 'label-sm gap-1.5 px-3 py-2 [&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:fill-current',
+        lg: 'label-lg gap-2.5 px-4 py-3 [&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:fill-current',
       },
     },
     defaultVariants: {
@@ -25,7 +29,7 @@ const chipVariants = cva(
   },
 );
 
-type ChipProps = ComponentProps<typeof BaseButton> &
+type ChipProps = ComponentPropsWithoutRef<typeof BaseButton> &
   VariantProps<typeof chipVariants> & {
     iconLeft?: ReactNode;
     iconRight?: ReactNode;
