6cfcab935ce6a3f6d882cd2f804eb0fdb6370efe
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-15 13:02:07 +0900

    chore: BottomSheet, Dialog export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,6 @@
+export { default as BottomSheet } from './components/BottomSheet';
 export { default as Button } from './components/Button';
+export { default as Dialog } from './components/Dialog';
 export { default as Field } from './components/Field';
 export { default as Input } from './components/Input';
 export { default as Textarea } from './components/Textarea';
