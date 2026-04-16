6fe88b9b4f69f1d258f426bc924f2393e85ab839
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-16 12:07:38 +0900

    chore: BottomNavigation export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
+export { default as BottomNavigation } from './components/BottomNavigation';
 export { default as BottomSheet } from './components/BottomSheet';
 export { default as Button } from './components/Button';
 export { default as Chip } from './components/Chip';
