a85e7fc39075149136994239d4b81c87ad3cc53f
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-21 04:27:31 +0900

    feat: 전역 Providers 컴포넌트 구현

---
+++ apps/web/src/shared/ui/Providers.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { type ReactNode, useState } from 'react';
+
+import { ToastProvider } from '@plog/ui';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider>{children}</ToastProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
