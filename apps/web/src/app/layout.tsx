30e449fafa618272d7ef154b24a09d637e81c1cb
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-21 04:27:34 +0900

    feat: RootLayout에 Providers, BottomTab 및 컨테이너 레이아웃 적용

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css';
 
+import { type ReactNode } from 'react';
+
 import type { Metadata } from 'next';
+
+import BottomTab from '@/shared/ui/BottomTab';
+import Providers from '@/shared/ui/Providers';
 
 export const metadata: Metadata = {
   title: 'plog',
@@ -10,11 +15,18 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <main className="mx-auto min-h-dvh w-full max-w-layout pb-bottom-tab shadow-[0px_0px_12px_0px_rgba(0,0,0,0.08)]">
+            {children}
+          </main>
+          <BottomTab />
+        </Providers>
+      </body>
     </html>
   );
 }
