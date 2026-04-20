f04b04a63fe13f9fe2e92acbe60f355fde1704ad
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-21 05:45:14 +0900

    fix: border 적용 오류 수정

--- packages/ui/src/components/Divider.tsx
+++ packages/ui/src/components/Divider.tsx
@@ -3,18 +3,50 @@ import { type ComponentPropsWithoutRef } from 'react';
 import { cn } from '@plog/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
-const dividerVariants = cva('border-semantic-object-subtler', {
+const dividerVariants = cva('border-0 border-semantic-object-subtler', {
   variants: {
     thickness: {
-      small: 'border-1',
-      medium: 'border-2',
-      large: 'border-3',
+      small: '',
+      medium: '',
+      large: '',
     },
     orientation: {
-      horizontal: 'border-t w-full',
-      vertical: 'border-l h-full',
+      horizontal: 'w-full',
+      vertical: 'h-full',
     },
   },
+  compoundVariants: [
+    {
+      thickness: 'small',
+      orientation: 'horizontal',
+      class: 'border-t-1',
+    },
+    {
+      thickness: 'medium',
+      orientation: 'horizontal',
+      class: 'border-t-2',
+    },
+    {
+      thickness: 'large',
+      orientation: 'horizontal',
+      class: 'border-t-3',
+    },
+    {
+      thickness: 'small',
+      orientation: 'vertical',
+      class: 'border-l-1',
+    },
+    {
+      thickness: 'medium',
+      orientation: 'vertical',
+      class: 'border-l-2',
+    },
+    {
+      thickness: 'large',
+      orientation: 'vertical',
+      class: 'border-l-3',
+    },
+  ],
   defaultVariants: {
     thickness: 'small',
     orientation: 'horizontal',
