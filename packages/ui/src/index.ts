36cac83be02bc8759bcf5cd24eb0ae2b81fabcc6
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-21 06:54:21 +0900

    chore: Badge export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
+export { default as Badge } from './components/Badge';
 export { default as BottomNavigation } from './components/BottomNavigation';
 export { default as BottomSheet } from './components/BottomSheet';
 export { default as Button } from './components/Button';
