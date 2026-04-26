2159581f8a2cde78747499c15fd2b48566b6921b
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-26 13:43:40 +0900

    chore: Kakao Maps SDK 전역 타입 선언 추가

--- apps/web/src/global.d.ts
+++ apps/web/src/global.d.ts
@@ -3,3 +3,22 @@ declare module '*.svg' {
   const ReactComponent: FC<SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+
+declare namespace kakao.maps {
+  class Map {
+    constructor(container: HTMLElement, options: MapOptions);
+    setCenter(latlng: LatLng): void;
+  }
+  class LatLng {
+    constructor(lat: number, lng: number);
+  }
+  interface MapOptions {
+    center: LatLng;
+    level: number;
+  }
+  function load(callback: () => void): void;
+}
+
+interface Window {
+  kakao: typeof kakao;
+}
