de490fdbf5fef4b448da46f9976c76b92708650f
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-26 16:36:12 +0900

    chore: Carousel export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -4,6 +4,7 @@ export * from './components/Badge';
 export * from './components/BottomNavigation';
 export * from './components/BottomSheet';
 export * from './components/Button';
+export * from './components/Carousel';
 export * from './components/Checkbox';
 export * from './components/Chip';
 export * from './components/DatePicker';
