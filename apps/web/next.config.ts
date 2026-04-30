e1a1082ccd8c44d281aa3ebae92ac4d1279bf402
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-04-30 19:08:15 +0900

    chore: SVG 로더 옵션 확장

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,7 +4,25 @@ const nextConfig: NextConfig = {
   turbopack: {
     rules: {
       '*.svg': {
-        loaders: ['@svgr/webpack'],
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
         as: '*.js',
       },
     },
