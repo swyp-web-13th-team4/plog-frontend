782c29b0f601e7b1c4654df1be8d381f0dda4740
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-05-02 00:21:19 +0900

    refactor: 하단 탭 레이아웃 컴포넌트 분리

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,8 +4,7 @@ import { type ReactNode } from 'react';
 
 import type { Metadata } from 'next';
 
-import { BottomTab } from '@/shared/ui';
-import { Providers } from '@/shared/ui';
+import Providers from '@/shared/ui/Providers';
 
 export const metadata: Metadata = {
   title: 'plog',
@@ -21,10 +20,9 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <Providers>
-          <main className="mx-auto min-h-dvh w-full max-w-layout pb-bottom-tab shadow-[0px_0px_12px_0px_rgba(0,0,0,0.08)]">
+          <main className="mx-auto min-h-dvh w-full max-w-layout shadow-[0px_0px_12px_0px_rgba(0,0,0,0.08)]">
             {children}
           </main>
-          <BottomTab />
         </Providers>
       </body>
     </html>
