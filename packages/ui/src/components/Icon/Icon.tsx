05ffadeb794bcf7bb7fe33402917287cb7b12679
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-05-03 04:41:22 +0900

    feat: Icon 컴포넌트 구현

---
+++ packages/ui/src/components/Icon/Icon.tsx
@@ -0,0 +1,42 @@
+import { cn } from '@plog/utils';
+
+import { iconMap, type IconName } from './iconMap';
+
+const BASE_SIZE = 24;
+
+type IconProps = {
+  name: IconName;
+  size?: number;
+  boxed?: boolean;
+  className?: string;
+};
+
+export default function Icon({
+  name,
+  size,
+  boxed = true,
+  className,
+}: IconProps) {
+  const { Component, width: rawWidth, height: rawHeight } = iconMap[name];
+
+  if (!boxed) {
+    return <Component width={size} height={size} className={className} />;
+  }
+
+  const containerSize = size ?? BASE_SIZE;
+  const scale = size !== undefined ? size / BASE_SIZE : 1;
+  const iconWidth = rawWidth * scale;
+  const iconHeight = rawHeight * scale;
+
+  return (
+    <span
+      className={cn(
+        'inline-flex shrink-0 items-center justify-center',
+        className,
+      )}
+      style={{ width: containerSize, height: containerSize }}
+    >
+      <Component width={iconWidth} height={iconHeight} />
+    </span>
+  );
+}
