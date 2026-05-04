715a1bb21feb9c078fe9f8182860f43ce5f169c8
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-05-05 06:13:52 +0900

    feat: 약관 페이지 메타데이터 추가

--- apps/web/app/terms/geolocation/page.tsx
+++ apps/web/app/terms/geolocation/page.tsx
@@ -4,6 +4,8 @@ import { JSDOM } from 'jsdom';
 import { getTerm } from '@/entities/user/api/server';
 import { TermsPage } from '@/views/terms';
 
+export const metadata = { title: '위치정보 이용약관' };
+
 export default async function Page() {
   const window = new JSDOM('').window;
   const purify = DOMPurify(window);
--- apps/web/app/terms/privacy/page.tsx
+++ apps/web/app/terms/privacy/page.tsx
@@ -4,6 +4,8 @@ import { JSDOM } from 'jsdom';
 import { getTerm } from '@/entities/user/api/server';
 import { TermsPage } from '@/views/terms';
 
+export const metadata = { title: '개인정보 처리방침' };
+
 export default async function Page() {
   const window = new JSDOM('').window;
   const purify = DOMPurify(window);
