72cad1bb93a9af7d838cc4c8124907906df7330a
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-05-05 05:33:56 +0900

    feat: 전역 에러 페이지 추가

---
+++ apps/web/app/global-error.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import { Button, EmptyState } from '@plog/ui';
+
+import ErrorGraphic from '@/shared/assets/empty-graphics/error.svg';
+
+export default function GlobalError({
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  return (
+    <div className="flex h-dvh items-center">
+      <EmptyState
+        graphic={<ErrorGraphic />}
+        title="서비스에 접속할 수 없습니다."
+        description={
+          '지금 이 서비스와 연결할 수 없습니다.\n문제를 해결하기 위해 노력하고 있습니다.\n잠시 후 다시 확인해 주세요.'
+        }
+        actions={
+          <div className="flex gap-3">
+            <Button
+              variant="outline"
+              size="small"
+              onClick={() => router.replace('/')}
+            >
+              메인 화면으로 이동
+            </Button>
+            <Button variant="outline" size="small" onClick={reset}>
+              다시 시도하기
+            </Button>
+          </div>
+        }
+        className="flex-1 justify-center py-12"
+      />
+    </div>
+  );
+}
