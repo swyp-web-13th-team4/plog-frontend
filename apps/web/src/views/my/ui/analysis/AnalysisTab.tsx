db851a3c2399545e1c3108c99ed9261d87e1ef94
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-05-07 20:47:11 +0900

    fix: 분석 데이터 null이면 잠김 UI로 폴백 및 로딩/에러 상태 분기 추가

--- apps/web/src/views/my/ui/analysis/AnalysisTab.tsx
+++ apps/web/src/views/my/ui/analysis/AnalysisTab.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import { useAnalyticsQuery } from '@/entities/user';
+import { type AnalyticsData, useAnalyticsQuery } from '@/entities/user';
 
 import AnalysisContent from './AnalysisContent';
 
+const LOCKED_DATA: AnalyticsData = {
+  totalPostCount: 0,
+  totalStudyTime: 0,
+  workType: null,
+  focusEnvironment: null,
+  spaceRankings: null,
+};
+
 export default function AnalysisTab() {
-  const { data } = useAnalyticsQuery();
+  const { data, isLoading, isError } = useAnalyticsQuery();
 
-  if (!data) return null;
+  // TODO: 디자인 확정 후 로딩 UI(스피너/스켈레톤) 적용
+  if (isLoading) return null;
 
-  return <AnalysisContent data={data} />;
+  // TODO: 디자인 확정 후 에러 UI 적용
+  if (isError) return null;
+
+  return <AnalysisContent data={data ?? LOCKED_DATA} />;
 }
