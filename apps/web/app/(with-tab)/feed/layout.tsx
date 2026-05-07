ee86b09ff81f4872d35ee7cdbef53f9a93678eed
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-05-08 05:14:46 +0900

    fix: 피드 레이아웃 수정

--- apps/web/app/(with-tab)/feed/layout.tsx
+++ apps/web/app/(with-tab)/feed/layout.tsx
@@ -28,10 +28,10 @@ export default function FeedLayout({ children }: FeedLayoutProps) {
 
   return (
     <>
-      <header>
+      <header className="fixed inset-x-0 top-0 z-10 mx-auto max-w-layout">
         <AppBar variant="navigation" title="피드" onBack={onBack} />
       </header>
-      {children}
+      <div className="pt-[var(--spacing-header)]">{children}</div>
     </>
   );
 }
