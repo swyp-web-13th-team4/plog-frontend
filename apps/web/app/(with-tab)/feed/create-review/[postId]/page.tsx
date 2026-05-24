1c763b98a40b879eceafb2738d24878826cdc0da
백승민 <jah02190@naver.com>
2026-05-24 12:13:27 +0900

    feat: 리뷰 작성 페이지 메타데이터 추가

--- apps/web/app/(with-tab)/feed/create-review/[postId]/page.tsx
+++ apps/web/app/(with-tab)/feed/create-review/[postId]/page.tsx
@@ -1,4 +1,8 @@
+import type { Metadata } from 'next';
+
 import { CreateReviewPage } from '@/views/create-review';
+
+export const metadata: Metadata = { title: '리뷰 작성' };
 
 export default async function Page({
   params,
