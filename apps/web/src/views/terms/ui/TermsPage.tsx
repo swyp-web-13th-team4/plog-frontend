e18ed75f73722b84a5c8ac663bf250516023d4f5
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-06-03 16:47:10 +0900

    fix: 약관 페이지 본문 텍스트 간격 수정

--- apps/web/src/views/terms/ui/TermsPage.tsx
+++ apps/web/src/views/terms/ui/TermsPage.tsx
@@ -27,7 +27,7 @@ export default function TermsPage({ title, content }: TermsPageProps) {
         dangerouslySetInnerHTML={{ __html: content }}
         className={cn(
           'body-sm min-h-dvh p-6 pt-[calc(24px+var(--spacing-header))] text-semantic-object-bold',
-          '[&_h2]:-mb-1 [&_h2]:text-semantic-title-md [&_h2]:leading-semantic-title-md [&_h2]:font-semantic-title-md',
+          '[&_h2]:-mb-1 [&_h2]:text-semantic-title-md [&_h2]:leading-semantic-title-md [&_h2]:font-semantic-title-md [&_h2+p]:pt-4 [&_p+p]:mt-2',
           '[&_h4]:pt-7 [&_h4]:pb-3 [&_h4]:text-semantic-label-lg [&_h4]:leading-semantic-label-lg [&_h4]:font-semantic-label-lg',
           '[&_ol]:list-decimal [&_ol]:pl-4 [&_ol_ol]:list-[lower-alpha]',
           '[&_hr]:-mx-6 [&_hr]:my-6 [&_hr]:border-4 [&_hr]:border-semantic-bg-deep',
