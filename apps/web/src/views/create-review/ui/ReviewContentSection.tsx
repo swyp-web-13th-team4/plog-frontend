bfc91df10bd0c8d40c78a6a35a12c747efabaf23
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-07-19 15:14:21 +0900

    refactor: 불필요한 div 래퍼 제거

--- apps/web/src/views/create-review/ui/ReviewContentSection.tsx
+++ apps/web/src/views/create-review/ui/ReviewContentSection.tsx
@@ -22,31 +22,27 @@ export default function ReviewContentSection({
 
   return (
     <section className="flex flex-col gap-6 px-6 pt-6 pb-10">
-      <div>
-        <Field label="더 자세한 후기를 남겨주세요">
-          <Textarea
-            {...contentsField}
-            value={reviewText}
-            maxLength={300}
-            placeholder={
-              '자유롭게 내용을 입력해 주세요. (300자 이내)\n부적절하거나 불쾌감을 줄 수 있는 내용은 제재를 받을 수 있습니다.'
-            }
-            className="h-40"
-          />
-        </Field>
-      </div>
-      <div>
-        <Field label="사진 등록">
-          <PhotoUploader
-            photos={photos}
-            onAdd={handleAddPhotos}
-            onRemove={handleRemovePhoto}
-            onFileSizeExceeded={handlePhotoFileSizeExceeded}
-            onMaxCountExceeded={handlePhotoMaxCountExceeded}
-            onConversionFailed={handlePhotoConversionFailed}
-          />
-        </Field>
-      </div>
+      <Field label="더 자세한 후기를 남겨주세요">
+        <Textarea
+          {...contentsField}
+          value={reviewText}
+          maxLength={300}
+          placeholder={
+            '자유롭게 내용을 입력해 주세요. (300자 이내)\n부적절하거나 불쾌감을 줄 수 있는 내용은 제재를 받을 수 있습니다.'
+          }
+          className="h-40"
+        />
+      </Field>
+      <Field label="사진 등록">
+        <PhotoUploader
+          photos={photos}
+          onAdd={handleAddPhotos}
+          onRemove={handleRemovePhoto}
+          onFileSizeExceeded={handlePhotoFileSizeExceeded}
+          onMaxCountExceeded={handlePhotoMaxCountExceeded}
+          onConversionFailed={handlePhotoConversionFailed}
+        />
+      </Field>
     </section>
   );
 }
