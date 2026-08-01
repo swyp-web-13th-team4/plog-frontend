73101f1a582050b6b1312ef50cca1e0e09b6fcd9
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-08-01 22:35:29 +0900

    refactor: review 엔티티 배럴에서 미사용 export 제거

--- apps/web/src/entities/review/index.ts
+++ apps/web/src/entities/review/index.ts
@@ -8,40 +8,20 @@ export {
 export { isReviewEditable } from './lib/is-review-editable';
 export {
   REVIEW_ENVIRONMENT_GROUPS,
-  REVIEW_ENVIRONMENT_ICON_NAMES,
   REVIEW_ENVIRONMENT_LABELS,
-  REVIEW_ENVIRONMENT_NAMES,
   REVIEW_ENVIRONMENT_SCORES,
   type ReviewEnvironmentGroup,
-  type ReviewEnvironmentIconName,
   type ReviewEnvironmentName,
   type ReviewEnvironmentScore,
 } from './model/environment';
 export { reviewQueryKeys } from './model/query-keys';
 export {
   type EditReviewResponse,
-  type PlaceReviewEnvironmentItem,
-  type PlaceReviewEnvironmentSummary,
   type PlaceReviewListItem,
-  type PlaceReviewPageItems,
-  type PlaceReviewPageResponse,
   type PlaceReviewSummary,
-  type ReviewResponse,
-} from './model/schemas';
-export {
-  editReviewResponseSchema,
-  placeReviewEnvironmentItemSchema,
-  placeReviewEnvironmentSummarySchema,
-  placeReviewListItemSchema,
-  placeReviewPageItemsSchema,
-  placeReviewPageResponseSchema,
-  placeReviewSummarySchema,
-  reviewEnvironmentNameSchema,
-  reviewResponseSchema,
 } from './model/schemas';
 export type {
   CreateReviewRequest,
-  GetPlaceReviewsRequest,
   ReviewSortType,
   UpdateReviewRequest,
 } from './model/types';
